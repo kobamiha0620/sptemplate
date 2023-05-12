3aeeafe9d5f68380fc09313ac340de4a57f2fe1d
M koba <mihatu.irisawa@twinplanet.co.jp>
2023-05-12 15:19:33 +0900

    バナー修正など

--- template_parts/bnrs.php
+++ template_parts/bnrs.php
@@ -15,9 +15,9 @@
     <a href="/consultation/">
         <img src="https://femtech.tv/wp-content/uploads/2022/01/bnr_onayami.png" alt="あなたのお悩み募集" width="100%">
     </a>
-    <a href="/wrecruit/">
+    <!-- <a href="/wrecruit/">
         <img src="https://femtech.tv/wp-content/uploads/2022/06/bnr_wrecruit.png" alt="ライター募集" width="100%">
-    </a>
+    </a> -->
 </div>
 
 
